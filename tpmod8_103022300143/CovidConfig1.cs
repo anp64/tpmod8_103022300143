1007a6211020ff99289ddcb9d4026ceba90c822c
anp64 <anico644612@gmail.com>
2025-04-13 21:25:11 +0700

    commit1

---
+++ tpmod8_103022300143/CovidConfig1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.IO;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; }
+    public int batas_hari_demam { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    public static CovidConfig LoadConfig(string path)
+    {
+        string json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<CovidConfig>(json);
+    }
+
+    public void UbahSatuan()
+    {
+        if (satuan_suhu.ToLower() == "celcius")
+        {
+            satuan_suhu = "fahrenheit";
+        }
+        else
+        {
+            satuan_suhu = "celcius";
+        }
+    }
+}
--- tpmod8_103022300143/Program.cs
+++ tpmod8_103022300143/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Load konfigurasi
+        CovidConfig config = CovidConfig.LoadConfig("covid_config.json");
+
+        // Input suhu
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        // Input hari
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        // Validasi suhu berdasarkan satuan
+        bool suhuValid = false;
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuValid = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            suhuValid = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        // Validasi hari
+        bool hariValid = hari < config.batas_hari_demam;
+
+        // Output sesuai kondisi
+        if (suhuValid && hariValid)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        // Contoh ubah satuan (jika mau digunakan)
+        // config.UbahSatuan();
+        // Console.WriteLine($"Satuan sekarang: {config.satuan_suhu}");
+    }
+}
